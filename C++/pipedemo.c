e546e28f3cd99d06cae87df23adb2af75097b8a0
ekel <ikelhe84@gmail.com>
2016-05-04 17:52:47 +0800

    pipe demo example

---
+++ C++/pipedemo.c
@@ -0,0 +1,55 @@
+/*
+ * pipedemo.c
+ *
+ *  Created on: 2016年5月4日
+ *      Author: zhiteng.he
+ */
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+//int pipe(int filedes[2]);
+//filedes[0]:read
+//filedes[1]:write
+//必须在fork()中调用pipe()，否则子进程不会继承文件描述符
+//两个进程不共享祖先进程，就不能使用pipe。但是可以使用命名管道
+int main()
+{
+    int result = -1;
+    int fd[2], nbytes;
+    pid_t pid;
+    char str[] = "my name is kingstar, now it's FIS!";
+    char readbuffer[100];
+    result = pipe(fd);
+    if (result == -1)
+    {
+        printf("failed to pipe!\n");
+        return -1;
+    }
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        printf("failed to fork!\n");
+        return -1;
+    }
+
+    if (pid == 0)   // son
+    {
+        close(fd[0]);
+        result = write(fd[1], str, strlen(str));
+        return 0;
+    }
+    else
+    {
+        close(fd[1]);
+        nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+        printf("the parent receive %d bytes data:%s\n", nbytes, readbuffer);
+
+    }
+    return 0;
+}
+
+
